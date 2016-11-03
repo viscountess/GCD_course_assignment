d15b704220d63613e1cea483b64accc0942c19da
viscountess <venerable_viscountess@hotmail.com>
2016-11-03 12:55:12 +0000

    Updating the main.cpp
    There is one part i need to fix - will do this later.
    Uploading just now to save

--- Games_Console_Dev Assignment/main.cpp
+++ Games_Console_Dev Assignment/main.cpp
@@ -10,12 +10,44 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
+	cout << "Parsing the image files......." << endl;
+
 	//Parsing the PGM data - am hoping this works.....
 	int row = 0, col = 0, numrows = 0, numcols = 0;
-	ifstream infile("image1.pgm");
+	ifstream inFile("image1.pgm");
 	stringstream ss;
 	string inputLine = "";
 
 	//First line:  Getting version
+	getline(inFile, inputLine);
+	if (inputLine.compare("P2") != 0) cerr << "PGM image version error." << endl;
+	else cout << "Version: " << inputLine << endl;
+
+	// Second line : comment
+	getline(inFile, inputLine);
+	cout << "Comment : " << inputLine << endl;
+
+	// Continue with a stringstream
+	ss << inFile.rdbuf();
+	// Third line : size
+	ss >> numcols >> numrows;
+	cout << numcols << " columns and " << numrows << " rows" << endl;
+
+	//int array[numrows][numcols]; //for some reason this isn't working - note to self need to fix
+
+	//// Following lines : data
+	//for (row = 0; row < numrows; ++row)
+	//	for (col = 0; col < numcols; ++col) ss >> array[row][col];
+
+	//// Now print the array to see the result
+	//for (row = 0; row < numrows; ++row) {
+	//	for (col = 0; col < numcols; ++col) {
+	//		cout << array[row][col] << " ";
+	//	}
+	//	cout << endl;
+	//}
+	inFile.close();
+
+	
 
 }
