23bd1e0d771b4896084d51762b6da53f4a25de29
viscountess <venerable_viscountess@hotmail.com>
2016-11-10 01:57:07 +0000

    Fixed image parser
    I have fixed the imager parser to parse 1 image and then I'd put it in
    seperate function.
    We may want to consider at a later stage, moving this to its own cpp
    file.

--- Games_Console_Dev Assignment/main.cpp
+++ Games_Console_Dev Assignment/main.cpp
@@ -7,14 +7,15 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+void imageParser(string fileName)
 {
-
 	cout << "Parsing the image files......." << endl;
 
 	//Parsing the PGM data - am hoping this works.....
-	int row = 0, col = 0, numrows = 0, numcols = 0;
-	ifstream inFile("image1.pgm");
+	int row = 0, col = 0, alpha = 0;
+	int numrows = 0;
+	int numcols = 0;
+	ifstream inFile(fileName.c_str());
 	stringstream ss;
 	string inputLine = "";
 
@@ -23,31 +24,43 @@ int main(int argc, char *argv[])
 	if (inputLine.compare("P2") != 0) cerr << "PGM image version error." << endl;
 	else cout << "Version: " << inputLine << endl;
 
-	// Second line : comment
-	getline(inFile, inputLine);
-	cout << "Comment : " << inputLine << endl;
-
 	// Continue with a stringstream
 	ss << inFile.rdbuf();
-	// Third line : size
+	// Second  line : size
 	ss >> numcols >> numrows;
-	cout << numcols << " columns and " << numrows << " rows" << endl;
+	cout << "Size: " << numcols << " columns and " << numrows << " rows" << endl;
 
-	//int array[numrows][numcols]; //for some reason this isn't working - note to self need to fix
+	//Third line : alpha
+	ss >> alpha;
+	if (alpha != 255) cerr << "PGM image alpha error." << endl;
+	else cout << "Alpha: " << alpha << endl;
 
-	//// Following lines : data
-	//for (row = 0; row < numrows; ++row)
-	//	for (col = 0; col < numcols; ++col) ss >> array[row][col];
+	int **array = new int *[numrows];
+	for (int i = 0; i < numrows; i++)
+	{
+		array[i] = new int[numcols];
+	}
 
-	//// Now print the array to see the result
+	// Following lines : data
+	for (row = 0; row < numrows; ++row)
+		for (col = 0; col < numcols; ++col) ss >> array[row][col];
+
+	//// Now print the array to see the result - just for testing the parsing.
 	//for (row = 0; row < numrows; ++row) {
 	//	for (col = 0; col < numcols; ++col) {
 	//		cout << array[row][col] << " ";
 	//	}
 	//	cout << endl;
 	//}
-	inFile.close();
 
-	
+	inFile.close();
+}
+
+int main(int argc, char *argv[])
+{
+
+	imageParser("image1.pgm");
+
+	return 0;
 
 }
