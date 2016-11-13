8ac26f9f0c3d68c3ffd578507e48b4234b03e0ac
viscountess <venerable_viscountess@hotmail.com>
2016-11-13 22:18:25 +0000

    Added Mark's stuff & changed some things around
    -Put the sobel filter in a seperate function
    -Change the 2d arrays into 1d array (this was because I couldn't
    remember how 2d arrays were stored in memory
    -Image buffer is passed into the sobel function.

--- Games_Console_Dev Assignment/main.cpp
+++ Games_Console_Dev Assignment/main.cpp
@@ -3,18 +3,15 @@
 #include <fstream>
 #include <sstream>
 
-//#include "pbma_io.hpp"
-
 using namespace std;
 
-void imageParser(string fileName)
+int *imageParser(string fileName, int &imageWidth, int &imageHeight)
 {
+
 	cout << "Parsing the image files......." << endl;
 
 	//Parsing the PGM data - am hoping this works.....
 	int row = 0, col = 0, alpha = 0;
-	int numrows = 0;
-	int numcols = 0;
 	ifstream inFile(fileName.c_str());
 	stringstream ss;
 	string inputLine = "";
@@ -27,39 +24,123 @@ void imageParser(string fileName)
 	// Continue with a stringstream
 	ss << inFile.rdbuf();
 	// Second  line : size
-	ss >> numcols >> numrows;
-	cout << "Size: " << numcols << " columns and " << numrows << " rows" << endl;
+	ss >> imageWidth >> imageHeight;
+	cout << "Size: " << imageWidth << " columns and " << imageHeight << " rows" << endl;
 
 	//Third line : alpha
 	ss >> alpha;
 	if (alpha != 255) cerr << "PGM image alpha error." << endl;
 	else cout << "Alpha: " << alpha << endl;
 
-	int **array = new int *[numrows];
-	for (int i = 0; i < numrows; i++)
+	int *array = new int [imageHeight * imageWidth];
+	/*for (int i = 0; i < imageHeight; i++)
 	{
-		array[i] = new int[numcols];
-	}
+		array[i] = new int[imageWidth];
+	}*/
 
 	// Following lines : data
-	for (row = 0; row < numrows; ++row)
-		for (col = 0; col < numcols; ++col) ss >> array[row][col];
-
+	/*for (row = 0; row < imageHeight; ++row)
+		for (col = 0; col < imageWidth; ++col) ss >> array[row][col];
+*/
+	for (int i = 0; i < imageHeight * imageWidth; i++)
+	{
+		ss >> array[i];
+	}
 	//// Now print the array to see the result - just for testing the parsing.
 	//for (row = 0; row < numrows; ++row) {
-	//	for (col = 0; col < numcols; ++col) {
-	//		cout << array[row][col] << " ";
-	//	}
-	//	cout << endl;
+	//for (col = 0; col < numcols; ++col) {
+	//cout << array[row][col] << " ";
+	//}
+	//cout << endl;
 	//}
 
 	inFile.close();
+
+	return array;
+
+}
+
+void applySobelFilter(string fileName, int *inputBuffer, int imageWidth, int imageHeight)
+{
+	int row = 0, col = 0;
+
+	//Output array
+	int *out = new int [imageHeight * imageWidth];
+	for (int i = 0; i < imageWidth * imageHeight; i++)
+	{
+		out[i] = 0;
+	}
+	/*for (int i = 0; i < imageHeight; i++)
+	{
+		out[i] = new int[imageWidth];
+		for (int p = 0; p < imageWidth; p++) {
+			out[i][p] = 0;
+
+		}
+	}*/
+
+	//cout << "Out: " << array[0][0] << ' ' << endl;
+
+	//SOBEL IMPLEMENTATION
+
+	//int sobel_x[3][3] = { {-1,0,1} , {-2,0,2} , { -1,0,1 } };
+	//int sobel_y[3][3] = { {-1,-2,-1} , {0,0,0} , {1,2,1} };
+
+	//The convolution required to 
+	int sobel_x[3][3] = { { 1,0,-1 },{ 2,0,-2 },{ 1,0,-1 } };
+	int sobel_y[3][3] = { { 1,2,1 },{ 0,0,0 },{ -1,-2,-1 } };
+
+	int pixel_value;
+
+	for (int y = 1; y < imageHeight - 1; y++) {
+		for (int x = 1; x < imageWidth - 1; x++) {
+
+			int sum_x = 0;
+			int sum_y = 0;
+
+			for (int m = -1; m <= 1; m++) {
+				for (int n = -1; n <= 1; n++) {
+					int tmp_x = x + m;
+					int tmp_y = y + n;
+					int index = (tmp_y * imageWidth) + tmp_x;
+					pixel_value = inputBuffer[index];
+					//cout << "Pixel: " << pixel_value << endl;
+					sum_x += pixel_value * sobel_x[m + 1][n + 1];
+					sum_y += pixel_value * sobel_y[m + 1][n + 1];
+
+				}
+			}
+			int sum = abs(sum_x) + abs(sum_y);
+			//cout << "Sum: " << sum << endl;
+
+			int index = (y * imageWidth) + x;
+			out[index] = sum;
+			//cout << "Out: " << out[x][y] << "Position: X: "<< x << " Y:"<< y <<  endl;
+
+
+		}
+	}
+
+	//Outputting the pgm image file
+	ofstream fs(fileName);
+	fs << "P2" << endl;
+	fs << imageWidth << " " << imageHeight << endl;
+	fs << "255" << endl;
+	for (int i = 0; i < imageWidth * imageHeight; i++) {
+			int aa = out[i];
+			//cout << aa << endl;
+			fs << aa;
+			fs << " ";
+		}
+	fs.close();
 }
 
 int main(int argc, char *argv[])
 {
+	int imageWidth, imageHeight;
+	int *imageBuffer = imageParser("image1.pgm", imageWidth, imageHeight);
+	applySobelFilter("sobel_applied.pgm", imageBuffer, imageWidth, imageHeight);
 
-	imageParser("image1.pgm");
 
 	return 0;
 
